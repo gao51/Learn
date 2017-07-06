f799e953a838068d1773e2bc52c8684f62e35307
gao <gaozh51@163.com>
2017-07-07 00:18:40 +0800

    first commit

---
+++ myfile/src/main/java/com/head/GameHelper.java
@@ -0,0 +1,17 @@
+package com.head;
+
+import java.util.ArrayList;
+
+/**
+ * Created by gao on 2017/4/12.
+ */
+public class GameHelper {
+    public ArrayList<String> placeDotCom(int x){
+
+        return null;
+    }
+    public String getUserInput(String x){
+        System.out.println(x);
+        return x;
+    }
+}
